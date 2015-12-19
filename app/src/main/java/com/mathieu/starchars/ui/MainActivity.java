96f27d005bc1e7e2d4b52e70eaa74e7d37f5463f
Mathieu <mathieu.maree@gmail.com>
2015-12-19 14:14:17 +0100

    Created welcome (= about) activity

--- app/src/main/java/com/mathieu/starchars/ui/MainActivity.java
+++ app/src/main/java/com/mathieu/starchars/ui/MainActivity.java
@@ -1,5 +1,7 @@
 package com.mathieu.starchars.ui;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startWelcomeActivity();
+    }
+
+    private void startWelcomeActivity() {
+        SharedPreferences prefs = getSharedPreferences(getPackageName(), MODE_PRIVATE);
+        boolean isFirstLaunch = prefs.getBoolean("isFirstLaunch", true);
+//        if (isFirstLaunch) {
+            prefs.edit().putBoolean("isFirstLaunch", false).apply();
+            startActivity(new Intent(this, WelcomeActivity.class));
+//        }
     }
 
     @Override
--- app/src/main/java/com/mathieu/starchars/ui/WelcomeActivity.java
+++ app/src/main/java/com/mathieu/starchars/ui/WelcomeActivity.java
@@ -1,10 +1,12 @@
 package com.mathieu.starchars.ui;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mathieu.starchars.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Project :    Star Chars
@@ -15,8 +17,15 @@ import com.mathieu.starchars.R;
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick(R.id.button_lets_go)
+    protected void onButtonLetsGoClick() {
+        finish();
     }
 }
